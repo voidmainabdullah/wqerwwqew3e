fb0f20e9abe4deedb49705b496c4f0fc4477d201
voidmainabdullah <abdullahnew101@gmail.com>
2025-08-19 00:23:01 +0500

    Updated NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { Cloud, Upload, Link as LinkIcon, File, Shield } from "lucide-react";
 
 const NotFound = () => {
   const location = useLocation();
@@ -18,6 +19,13 @@ const NotFound = () => {
 
       {/* Glow Effect */}
       <div className="absolute w-[500px] h-[500px] bg-blue-600/20 rounded-full blur-3xl animate-pulse-slow"></div>
+
+      {/* Flying File-Sharing Icons */}
+      <Cloud className="icon-fly left-10 top-20 text-blue-500/30" />
+      <Upload className="icon-fly left-1/3 top-40 text-gray-400/30 delay-1000" />
+      <LinkIcon className="icon-fly right-20 top-60 text-blue-400/30 delay-500" />
+      <File className="icon-fly left-1/4 bottom-20 text-gray-500/30 delay-2000" />
+      <Shield className="icon-fly right-1/3 bottom-40 text-blue-600/30 delay-1500" />
 
       {/* Main Content */}
       <div className="relative text-center z-10 animate-fadeIn">
