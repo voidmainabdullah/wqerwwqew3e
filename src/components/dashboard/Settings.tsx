89aaa25efa72898d0e19472a90c9d98f3f4b62a1
voidmainabdullah <abdullahnew101@gmail.com>
2025-08-27 14:44:06 +0500

    Updated Settings.tsx

--- src/components/dashboard/Settings.tsx
+++ src/components/dashboard/Settings.tsx
@@ -114,7 +114,7 @@ export const Settings: React.FC = () => {
         </p>
       </div>
 
-      <div className="grid gap-4 w-90 h-10"> 
+      <div className="grid gap-4  h-10"> 
         <Card>
           <CardHeader>
             <CardTitle className="flex items-center">
