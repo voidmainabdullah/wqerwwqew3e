4e5392d87d97f43e3b9147774a54ced79c3fbb53
voidmainabdullah <abdullahnew101@gmail.com>
2025-08-27 12:33:54 +0500

    Updated FileManager.tsx

--- src/components/files/FileManager.tsx
+++ src/components/files/FileManager.tsx
@@ -116,7 +116,7 @@ export function FileManager() {
           <select
             value={filterType}
             onChange={(e) => setFilterType(e.target.value)}
-            className="px-3 py-2  rounded-xl bg-neutral-400 text-gray-500 focus:outline-none focus:ring-2 focus:ring-black dark:bg-neutral-600 dark:text-white dark:focus:ring-neutral-800"
+            className="px-3 py-2  rounded-xl bg-neutral-400 text-white focus:outline-none focus:ring-2 focus:ring-black dark:bg-neutral-600 dark:text-white dark:focus:ring-neutral-800"
           >
             <option value="all">All Types</option>
             <option value="image">Images</option>
