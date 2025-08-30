700bba0f9146816fbc8ce06587dbaef7510549f6
voidmainabdullah <abdullahnew101@gmail.com>
2025-08-30 17:27:30 +0500

    Updated Dashboard.tsx

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -173,7 +173,7 @@ export const Dashboard: React.FC = () => {
               className="group block transition-all duration-300 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 rounded-xl"
             >
               <div className="relative overflow-hidden rounded-xl  shadow-lg 
-                  rounded-xl p-4 bg-gradient-to-tr from-neutral-800 via-neutral-800 to-amber-600/80 p-4 shadow-sm rounded-xl">
+                  rounded-xl p-4 bg-gradient-to-tr from-neutral-800 via-neutral-800 to-amber-300/80 p-4 shadow-sm rounded-xl">
                 <div className="absolute inset-0 bg-gradient-to-r from-primary/10 to-accent/10 opacity-40"></div>
                 <div className="relative">
                   <div className="flex items-center gap-3 mb-2">
