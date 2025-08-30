7b3531d02bc11e3e18c68a7659e9b2b661f43dd2
voidmainabdullah <abdullahnew101@gmail.com>
2025-08-30 20:27:34 +0500

    Updated DashboardLayout.tsx

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -233,6 +233,20 @@ export const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) =>
                         <Gear className="mr-2 h-4 w-4" />
                         Settings
                       </Link>
+                       </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem asChild>
+                      <Link to="/dashboard/settings">
+                        <Gear className="mr-2 h-4 w-4" />
+                        Settings
+                      </Link>
+                       </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem asChild>
+                      <Link to="/dashboard/settings">
+                        <Gear className="mr-2 h-4 w-4" />
+                        Settings
+                      </Link>
                     </DropdownMenuItem>
                     <DropdownMenuSeparator />
                     <DropdownMenuItem onClick={() => signOut()}>
@@ -243,6 +257,7 @@ export const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) =>
                 </DropdownMenu>
               </div>
             </div>
+          
           </header>
 
           {/* Main Content */}
