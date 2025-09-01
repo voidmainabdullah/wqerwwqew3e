f0fd9c32f1106ae2cea02cdd9f3971f4bb3d2f9c
voidmainabdullah <abdullahnew101@gmail.com>
2025-09-02 01:00:44 +0500

    Updated DashboardLayout.tsx

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -68,7 +68,7 @@ const AppSidebar = () => {
             className="h-12 w-auto sm:h-14 md:h-16 object-contain"
           />
           <span className="font-bold text-xl bg-gradient-to-r from-foreground to-foreground/70 bg-clip-text">
-            SecureShare
+            
           </span>
         </div>
       </SidebarHeader>
