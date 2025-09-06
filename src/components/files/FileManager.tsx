446c228b65beb6b02cf2455619bb59f734bcf59d
voidmainabdullah <abdullahnew101@gmail.com>
2025-09-06 14:19:59 +0500

    Updated FileManager.tsx

--- src/components/files/FileManager.tsx
+++ src/components/files/FileManager.tsx
@@ -227,9 +227,9 @@ export function FileManager() {
 
       {/* Search and Filter */}
       <Card>
-        <CardContent className="p-6 bg-transparent">
+        <CardContent className="p-6 bg-neutral-800">
           <div className="flex flex-col sm:flex-row gap-4">
-            <div className="relative flex-1">
+            <div className="relative flex-1"> 
               <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
               <Input placeholder="Search files..." value={searchTerm} onChange={e => setSearchTerm(e.target.value)} className="pl-8 bg-neutral-700 rounded-2xl" />
             </div>
