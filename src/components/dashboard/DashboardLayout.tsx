deb4c77bc76d2bbcf6a067874dfcb415c9645f4d
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-12 14:44:13 +0500

    Updated DashboardLayout.tsx

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -414,7 +414,7 @@ export const DashboardLayout: React.FC<DashboardLayoutProps> = ({
 
                      <DropdownMenuItem asChild className="cursor-pointer hover:bg-accent focus:bg-accent">
                       <Link to="/dashboard/analytics" className="flex items-center gap-3 py-2.5">
-                        <ChartBar className="h-4 w-4 text-primary" weight="duotone" />
+                        <HardDrives className="h-4 w-4 text-primary" weight="duotone" />
                         <span className="font-body">Storage</span>
                       </Link>
                     </DropdownMenuItem>
