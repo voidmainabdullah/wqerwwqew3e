13c9fd84b952af6af718db1c2c260a46d8847e45
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-14 01:06:57 +0500

    Updated Dashboard.tsx

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -197,6 +197,7 @@ export const Dashboard: React.FC = () => {
 </div>
           </div>
         </div>
+      </div>
 
       <div className="p-8 space-y-8 bg-zinc-900">
         {/* Main Stats Grid */}
