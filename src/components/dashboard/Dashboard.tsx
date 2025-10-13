aa2420f97aff3e1d734b83e95a758365baf735e1
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-14 01:27:22 +0500

    Updated Dashboard.tsx

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -240,6 +240,9 @@ export const Dashboard: React.FC = () => {
   <div className="absolute -top-10 -right-10 w-32 h-32 bg-neutral-500/20 rounded-full blur-3xl animate-pulse-slow"></div>
   <div className="absolute bottom-0 left-0 w-40 h-40 bg-neutral-400/10 rounded-full blur-3xl animate-pulse-slow delay-1500"></div>
 </div>
+          </div>
+        </div>
+      </div>
 
 <style>
 {`
