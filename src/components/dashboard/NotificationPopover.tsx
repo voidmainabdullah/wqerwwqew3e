8b1a03982103075ded6aaec218899689ac0f9b44
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-14 14:14:42 +0500

    Updated NotificationPopover.tsx

--- src/components/dashboard/NotificationPopover.tsx
+++ src/components/dashboard/NotificationPopover.tsx
@@ -151,7 +151,7 @@ export const NotificationPopover: React.FC = () => {
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button variant="ghost" size="icon" className="relative">
-          <Notification className="h-5 w-5 hiiden:md" weight="duotone" />
+          <Notification className="h-5 w-5 hiiden:md" weight="fill" />
           {unreadCount > 0 && ( 
             <Badge 
               className="absolute -top-1 -right-1 h-5 w-5 flex items-center justify-center p-0 text-xs"
