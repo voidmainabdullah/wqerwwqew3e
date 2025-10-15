979139385be589769ef706841cb1ef0eefc25d5d
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-15 23:32:07 +0500

    Updated Dashboard.tsx

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -400,7 +400,7 @@ export const Dashboard: React.FC = () => {
           </Card>
 
           {/* Storage Card */}
-          <Card className="bg-gradient-to-br from-blue-900-40 to-blue-900/40 border border-zinc-700/40  hover:border-slate-500/50 transition-all duration-300">
+          <Card className="bg-gradient-to-br from-blue-900/40 to-blue-900/40 border border-zinc-700/40  hover:border-slate-500/50 transition-all duration-300">
             <CardHeader className="flex flex-row items-center justify-between pb-3">
               <div>
                 <CardTitle className="text-slate-400 text-sm font-medium uppercase tracking-wide">
