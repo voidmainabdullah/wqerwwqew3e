20654990357441694c7f431cee8f74b7f4aadf9c
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-15 22:56:43 +0500

    Updated card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg bg-red-600 text-card-foreground shadow-sm",
+      "rounded-lg bg-red-600 h-20 text-card-foreground shadow-sm",
       className
     )}
     {...props}
