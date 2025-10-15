fceb23811d73368db812a2c3b019c582e660444f
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-15 23:00:40 +0500

    Updated SharedLinks.tsx

--- src/components/files/SharedLinks.tsx
+++ src/components/files/SharedLinks.tsx
@@ -319,7 +319,7 @@ export const SharedLinks: React.FC = () => {
                     </div>
                   </div>
 
-                  {link.message && <div className="p-3 rounded-2xl border bg-blue-600/10 ">
+                  {link.message && <div className="p-3 rounded-2xl border bg-blue-800/10 ">
                       <p className="text-xs md:text-sm text-muted-foreground break-words">
                         <span className="font-medium">Message:</span> {link.message}
                       </p>
