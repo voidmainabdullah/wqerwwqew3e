72ce8968f3538c66e89577fafbb4ce036a5a2d06
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-18 20:45:24 +0500

    Updated Dashboard.tsx

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -122,7 +122,7 @@ export const Dashboard: React.FC = () => {
   return <div className="min-h-screen text-white rounded-xl bg-inherit">
      {/* Professional Header */}
     <div className="border-b border-neutral-800 bg-neutral-950/80 backdrop-blur-md rounded-full">
-  <div className="px-8 py-6 rounded-xl border border-[#ffffff10] bg-[#1c1917]">
+  <div className="px-6 py-6 rounded-xl border border-[#ffffff10] bg-[#1c1917]">
     <div className="flex items-center justify-between">
       {/* Dashboard Title */}
       <div className=" md:block space-y-1">
