80535333295c60b6c61cb85c275a37b90cf176e5
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-25 23:41:34 +0500

    Updated TeamsManager.tsx

--- src/components/teams/TeamsManager.tsx
+++ src/components/teams/TeamsManager.tsx
@@ -328,7 +328,7 @@ export function TeamsManager({ onSelectTeam }: TeamsManagerProps = {}) {
                       </CardDescription>
                     </div>
                   </div>
-                  <div className="flex gap-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                  <div className="flex gap-1 opacity-100 group-hover:opacity-100 transition-opacity">
                     <Button
                       variant="ghost"
                       size="sm"
