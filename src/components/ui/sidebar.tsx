42adaa74436ab06c1ab36bd58205d48c04574a9b
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-26 14:53:44 +0500

    Updated sidebar.tsx

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -232,7 +232,7 @@ const SidebarGroup = React.forwardRef<HTMLDivElement, React.ComponentProps<"div"
   className,
   ...props
 }, ref) => {
-  return <div ref={ref} data-sidebar="group" className={cn("relative flex w-full min-w-0 flex-col p-2", className)} {...props} />;
+  return <div ref={ref} data-sidebar="group" className={cn("relative flex w-full min-w-0 flex-col p-0", className)} {...props} />;
 });
 SidebarGroup.displayName = "SidebarGroup";
 const SidebarGroupLabel = React.forwardRef<HTMLDivElement, React.ComponentProps<"div"> & {
