be471eb649bb00ee09c2dc1ab636493a62f18c86
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-26 19:40:48 +0500

    Updated Settings.tsx

--- src/components/dashboard/Settings.tsx
+++ src/components/dashboard/Settings.tsx
@@ -14,7 +14,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
-import { Mail, CloudArrowUp, FileText,  Info, Shield } from "phosphorus-react";
+import { Mail, CloudArrowUp, FileText,  Info, Shield } from "lucide-react";
 
 export const FileBackupPage: React.FC = () => {
   const { user } = useAuth();
