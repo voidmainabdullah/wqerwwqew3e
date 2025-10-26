4018ae7846d56bda7f10c7633d29a2846288025d
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-26 15:24:16 +0500

    Updated sidebar.tsx

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -254,7 +254,7 @@ const SidebarGroupAction = React.forwardRef<HTMLButtonElement, React.ComponentPr
   ...props
 }, ref) => {
   const Comp = asChild ? Slot : "button";
-  return <Comp ref={ref} data-sidebar="group-action" className={cn("absolute right-3 top-3.5 flex aspect-square w-5 items-center justify-center rounded-md p-0 text-sidebar-foreground outline-none ring-sidebar-ring transition-transform hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
+  return <Comp ref={ref} data-sidebar="group-action" className={cn("absolute right-3 top-3.5 flex aspect-square w-5 items-center justify-center rounded-md p-0 text-sidebar-foreground outline-none ring-sidebar-ring transition-transform hover:bg-red-600 hover:text-sidebar-accent-foreground focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
   // Increases the hit area of the button on mobile.
   "after:absolute after:-inset-2 after:md:hidden", "group-data-[collapsible=icon]:hidden", className)} {...props} />;
 });
