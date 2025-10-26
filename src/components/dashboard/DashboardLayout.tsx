9bfedf384d033b5d561a24972db1a05b093969f7
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-26 14:59:28 +0500

    Updated DashboardLayout.tsx

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -88,8 +88,8 @@ const AppSidebar = () => {
         {/* Navigation */}
         <SidebarGroup>
           <SidebarGroupLabel className="text-xs font-heading font-semibold text-muted-foreground uppercase tracking-wider px-2 mb-1">
-            Navigation |
-            <span className="text-xs font-heading  font-semibold  text-neutral-600 uppercase tracking-wider px-2 mb-1">(ctrl + B)</span>
+            Navigation 
+            <span className="text-xs font-heading  font-semibold  text-neutral-600 uppercase tracking-wider px-2 mb-1">| (ctrl + B)</span>
           </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu className="space-y-0.5">
