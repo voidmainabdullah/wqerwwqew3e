48ad74336bfb5585038a792ef81d92ad61278ff0
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-26 19:06:11 +0500

    Updated Dashboard.tsx

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -121,8 +121,8 @@ export const Dashboard: React.FC = () => {
   const storageProgress = stats && !isPro ? stats.storageUsed / stats.storageLimit * 100 : 0;
   return <div className="min-h-screen text-white bg-stone-950 rounded-xl">
   {/* Professional Header */}
-  <div className="w-full border-b border-neutral-800 bg-red-400 backdrop-blur-md rounded-t-xl">
-    <div className="w-full border border-[#ffffff10] rounded-none bg-stone-950 py-2 sm:py-[8px] px-3 sm:px-6 md:px-6 -mt-8">
+  <div className="w-full border-b border-neutral-800 bg-neutral-950/8 backdrop-blur-md rounded-t-xl">
+    <div className="w-full border border-[#ffffff10] rounded-none bg-red-950 py-2 sm:py-[8px] px-3 sm:px-6 md:px-6 -mt-8">
       <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between w-full mx-0 my-0 px-0 gap-2 sm:gap-0">
         {/* Dashboard Title */}
         <div className="space-y-1">
