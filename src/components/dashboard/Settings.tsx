d4221d63c4f3e3a59423e8a3e3ffd3abe24de2c9
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-26 19:58:14 +0500

    Updated Settings.tsx

--- src/components/dashboard/Settings.tsx
+++ src/components/dashboard/Settings.tsx
@@ -281,7 +281,7 @@ export const Settings: React.FC = () => {
               >
                 {backupLoading ? (
                   <>
-                    <span className="animate-spin h-20">◌</span> Processing...
+                    <span className="animate-spin ">◌</span> Processing...
                   </>
                 ) : (
                   <>
