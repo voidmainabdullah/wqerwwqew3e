2600ab25deb2379df8d0af93799bb65090b59ad6
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-29 04:26:53 +0500

    Updated DashboardLayout.tsx

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -145,7 +145,7 @@ const AppSidebar = () => {
   <Link to="/subscription" className="flex items-center gap-2">
     <DiamondsFour className="w-4 h-4 flex-shrink-0 text-primary group-data-[collapsible=icon]:text-2xl" />
     <span className="font-body text-sm group-data-[collapsible=icon]:hidden">
-      Upgrade
+      Upgrade Plan
     </span>
                   </Link>
                 </SidebarMenuButton>
