84ecc348697b86c1d1035475cca31dc3d68c55f5
voidmainabdullah <abdullahnew101@gmail.com>
2025-10-30 22:51:02 +0500

    Updated HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ import { useTheme } from "@/contexts/ThemeContext";
 const HeroSection = () => {
   const { actualTheme } = useTheme();
   const [isVisible, setIsVisible] = useState(false);
-  const { scrollY } = useScroll();  
+  const { scrollY } = useScroll();
 
   const glowWidth = useTransform(scrollY, [0, 300], ["0%", "100%"]);
   const glowOpacity = useTransform(scrollY, [0, 300], [0, 1]);
@@ -231,130 +231,52 @@ const HeroSection = () => {
   );
 };
 
-/* ================== Card Component ================== */
+/* ================== Card Component (Converted to Tailwind) ================== */
 const Card = () => {
   return (
-    <StyledWrapper>
-      <div className="outer">
-        <div className="dot" />
-        <div className="card">
-          <div className="ray" />
-          <div className="text">750k</div>
-          <div>Views</div>
-          <div className="line topl" />
-          <div className="line leftl" />
-          <div className="line bottoml" />
-          <div className="line rightl" />
+    <div className="relative w-[300px] h-[250px] rounded-xl p-[1px] bg-gradient-radial from-gray-100 via-gray-800 to-black">
+      {/* Animated Dot */}
+      <div className="absolute w-[5px] aspect-square bg-white rounded-full shadow-[0_0_10px_#ffffff] animate-[moveDot_6s_linear_infinite]"></div>
+
+      {/* Inner Card */}
+      <div className="relative w-full h-full flex flex-col items-center justify-center rounded-lg border border-neutral-800 bg-gradient-radial from-gray-500 to-black text-white overflow-hidden">
+        <div className="absolute w-[220px] h-[45px] bg-gray-200 opacity-40 rounded-full blur-md rotate-[40deg] top-0 left-0"></div>
+        <div className="text-6xl font-extrabold bg-gradient-to-r from-black via-white to-black bg-clip-text text-transparent">
+          750k
         </div>
+        <div>Views</div>
+
+        {/* Lines */}
+        <div className="absolute top-[10%] left-0 w-full h-[1px] bg-gradient-to-r from-gray-400 to-gray-800"></div>
+        <div className="absolute bottom-[10%] left-0 w-full h-[1px] bg-gray-700"></div>
+        <div className="absolute left-[10%] top-0 h-full w-[1px] bg-gradient-to-b from-gray-500 to-gray-800"></div>
+        <div className="absolute right-[10%] top-0 h-full w-[1px] bg-gray-800"></div>
       </div>
-    </StyledWrapper>
+
+      {/* Animation Keyframes */}
+      <style jsx>{`
+        @keyframes moveDot {
+          0%,
+          100% {
+            top: 10%;
+            right: 10%;
+          }
+          25% {
+            top: 10%;
+            right: calc(100% - 35px);
+          }
+          50% {
+            top: calc(100% - 30px);
+            right: calc(100% - 35px);
+          }
+          75% {
+            top: calc(100% - 30px);
+            right: 10%;
+          }
+        }
+      `}</style>
+    </div>
   );
-}
-
-const StyledWrapper = styled.div`
-  .outer {
-    width: 300px;
-    height: 250px;
-    border-radius: 10px;
-    padding: 1px;
-    background: radial-gradient(circle 230px at 0% 0%, #ffffff, #0c0d0d);
-    position: relative;
-  }
-
-  .dot {
-    width: 5px;
-    aspect-ratio: 1;
-    position: absolute;
-    background-color: #fff;
-    box-shadow: 0 0 10px #ffffff;
-    border-radius: 100px;
-    z-index: 2;
-    right: 10%;
-    top: 10%;
-    animation: moveDot 6s linear infinite;
-  }
-
-  @keyframes moveDot {
-    0%,
-    100% {
-      top: 10%;
-      right: 10%;
-    }
-    25% {
-      top: 10%;
-      right: calc(100% - 35px);
-    }
-    50% {
-      top: calc(100% - 30px);
-      right: calc(100% - 35px);
-    }
-    75% {
-      top: calc(100% - 30px);
-      right: 10%;
-    }
-  }
-
-  .card {
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-    border-radius: 9px;
-    border: solid 1px #202222;
-    background-size: 20px 20px;
-    background: radial-gradient(circle 280px at 0% 0%, #444444, #0c0d0d);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    flex-direction: column;
-    color: #fff;
-  }
-  .ray {
-    width: 220px;
-    height: 45px;
-    border-radius: 100px;
-    position: absolute;
-    background-color: #c7c7c7;
-    opacity: 0.4;
-    box-shadow: 0 0 50px #fff;
-    filter: blur(10px);
-    transform-origin: 10%;
-    top: 0%;
-    left: 0;
-    transform: rotate(40deg);
-  }
-
-  .card .text {
-    font-weight: bolder;
-    font-size: 4rem;
-    background: linear-gradient(45deg, #000000 4%, #fff, #000);
-    background-clip: text;
-    color: transparent;
-  }
-
-  .line {
-    width: 100%;
-    height: 1px;
-    position: absolute;
-    background-color: #2c2c2c;
-  }
-  .topl {
-    top: 10%;
-    background: linear-gradient(90deg, #888888 30%, #1d1f1f 70%);
-  }
-  .bottoml {
-    bottom: 10%;
-  }
-  .leftl {
-    left: 10%;
-    width: 1px;
-    height: 100%;
-    background: linear-gradient(180deg, #747474 30%, #222424 70%);
-  }
-  .rightl {
-    right: 10%;
-    width: 1px;
-    height: 100%;
-  }`;
+};
 
 export default HeroSection;
