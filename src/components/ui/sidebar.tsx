40e2cc72f090970b2e17fa2916b56ec0cc34fd4a
voidmainabdullah <abdullahnew101@gmail.com>
2025-11-01 01:33:31 +0500

    Updated sidebar.tsx

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -211,7 +211,7 @@ const SidebarFooter = React.forwardRef<HTMLDivElement, React.ComponentProps<"div
   className,
   ...props
 }, ref) => {
-  return <div ref={ref} data-sidebar="footer" className={cn("flex flex-col gap-2 p-2", className)} {...props} />;
+  return <div ref={ref} data-sidebar="footer" className={cn("flex flex-col gap-2 p-6", className)} {...props} />;
 });
 SidebarFooter.displayName = "SidebarFooter";
 const SidebarSeparator = React.forwardRef<React.ElementRef<typeof Separator>, React.ComponentProps<typeof Separator>>(({
